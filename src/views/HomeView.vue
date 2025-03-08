a5b0ec46f409bc63c147c26f9dba5b714575b068
HIL88 <hilserrano1520@gmail.com>
2025-03-07 21:52:23 -0600

    agregando estilos

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="home-container">
     <h1>Bienvenido a la Gestión de Productos</h1>
     <p>Utiliza el menú de navegación para administrar tus productos.</p>
   </div>
@@ -10,3 +10,20 @@ export default {
   name: 'Home'
 };
 </script>
+<style scoped>
+.home-container {
+  height: calc(100vh - 110px); 
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  background-image: url('@/assets/images/fondo1.jpg'); 
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+  color: white;
+  text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.5);
+  width: 100%;
+}
+
+</style>
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,12 +1,14 @@
 <template>
-    <div>
+   <div class="login-container">
+    <div class="login-box">
       <h2>Iniciar Sesión</h2>
       <form @submit.prevent="login">
-        <input v-model="credentials.email" placeholder="Correo" type="email" />
-        <input v-model="credentials.password" placeholder="Contraseña" type="password" />
-        <button type="submit">Ingresar</button>
+        <input v-model="credentials.email" placeholder="Correo electrónico" type="email" class="input-field" required />
+        <input v-model="credentials.password" placeholder="Contraseña" type="password" class="input-field" required />
+        <button type="submit" class="login-button">Ingresar</button>
       </form>
     </div>
+  </div>
   </template>
   
   <script>
@@ -33,4 +35,53 @@
     }
   };
   </script>
-  
+  <style scoped>
+  /* Estilos generales */
+  .login-container {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    height: calc(100vh - 110px);
+    background: linear-gradient(to right, #4a90e2, #1459a5); 
+  }
+  .login-box {
+    background: white;
+    padding: 30px;
+    border-radius: 10px;
+    box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.2);
+    text-align: center;
+    width: 350px;
+  }
+  h2 {
+    margin-bottom: 20px;
+    color: #333;
+  }
+  .input-field {
+    width: 100%;
+    padding: 10px;
+    margin: 10px 0;
+    border: 1px solid #ccc;
+    border-radius: 5px;
+    font-size: 16px;
+    transition: 0.3s;
+  }
+
+  .input-field:focus {
+    border-color: #4a90e2;
+    outline: none;
+  }
+  .login-button {
+    width: 100%;
+    padding: 10px;
+    background: #4a90e2;
+    border: none;
+    color: white;
+    font-size: 18px;
+    border-radius: 5px;
+    cursor: pointer;
+    transition: 0.3s;
+  }
+  .login-button:hover {
+    background: #357ac5;
+  }
+  </style>
